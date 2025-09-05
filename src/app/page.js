3f7407954010cb24499c36070de61d78f9b15c27
Eurico Sá Fernandes <1779775+eeeurico@users.noreply.github.com>
2025-09-05 16:32:31 +0200

    improve images

--- src/app/page.js
+++ src/app/page.js
@@ -1,73 +1,49 @@
-// frames
-import frames1 from "../images/frames1.png"
-import frames2 from "../images/frames2.png"
-import frames3 from "../images/frames3.png"
-import frames4 from "../images/frames4.png"
-import frames5 from "../images/frames5.png"
-import frames6 from "../images/frames6.png"
-import frames7 from "../images/frames7.png"
-import frames8 from "../images/frames8.png"
-import frames9 from "../images/frames9.png"
-import frames10 from "../images/frames10.png"
-import frames11 from "../images/frames11.png"
-import frames12 from "../images/frames12.png"
-import frames13 from "../images/frames13.png"
-import frames14 from "../images/frames14.png"
-import frames15 from "../images/frames15.png"
-import frames16 from "../images/frames16.png"
-import frames17 from "../images/frames17.png"
-import frames18 from "../images/frames18.png"
-import frames19 from "../images/frames19.png"
-import frames20 from "../images/frames20.png"
-import frames21 from "../images/frames21.png"
-import frames22 from "../images/frames22.png"
-
 // frames text
-import framesText1 from "../images/_frames-text1.png"
-import framesText2 from "../images/_frames-text2.png"
-import framesText3 from "../images/_frames-text3.png"
-import framesText4 from "../images/_frames-text4.png"
-import framesText5 from "../images/_frames-text5.png"
-import framesText6 from "../images/_frames-text6.png"
-import framesText7 from "../images/_frames-text7.png"
-import framesText8 from "../images/_frames-text8.png"
-import framesText9 from "../images/_frames-text9.png"
-import framesText10 from "../images/_frames-text10.png"
-import framesText11 from "../images/_frames-text11.png"
-import framesText12 from "../images/_frames-text12.png"
-import framesText13 from "../images/_frames-text13.png"
-import framesText14 from "../images/_frames-text14.png"
-import framesText15 from "../images/_frames-text15.png"
-import framesText16 from "../images/_frames-text16.png"
-import framesText17 from "../images/_frames-text17.png"
-import framesText18 from "../images/_frames-text18.png"
-import framesText19 from "../images/_frames-text19.png"
-import framesText20 from "../images/_frames-text20.png"
-import framesText21 from "../images/_frames-text21.png"
-import framesText22 from "../images/_frames-text22.png"
+import framesText1 from "../images-sm/_frames-text1.webp"
+import framesText2 from "../images-sm/_frames-text2.webp"
+import framesText3 from "../images-sm/_frames-text3.webp"
+import framesText4 from "../images-sm/_frames-text4.webp"
+import framesText5 from "../images-sm/_frames-text5.webp"
+import framesText6 from "../images-sm/_frames-text6.webp"
+import framesText7 from "../images-sm/_frames-text7.webp"
+import framesText8 from "../images-sm/_frames-text8.webp"
+import framesText9 from "../images-sm/_frames-text9.webp"
+import framesText10 from "../images-sm/_frames-text10.webp"
+import framesText11 from "../images-sm/_frames-text11.webp"
+import framesText12 from "../images-sm/_frames-text12.webp"
+import framesText13 from "../images-sm/_frames-text13.webp"
+import framesText14 from "../images-sm/_frames-text14.webp"
+import framesText15 from "../images-sm/_frames-text15.webp"
+import framesText16 from "../images-sm/_frames-text16.webp"
+import framesText17 from "../images-sm/_frames-text17.webp"
+import framesText18 from "../images-sm/_frames-text18.webp"
+import framesText19 from "../images-sm/_frames-text19.webp"
+import framesText20 from "../images-sm/_frames-text20.webp"
+import framesText21 from "../images-sm/_frames-text21.webp"
+import framesText22 from "../images-sm/_frames-text22.webp"
 // inside
-import inside1 from "../images/_inside1.jpg"
-import inside2 from "../images/_inside2.jpg"
-import inside3 from "../images/_inside3.jpg"
-import inside4 from "../images/_inside4.jpg"
-import inside5 from "../images/_inside5.jpg"
-import inside6 from "../images/_inside6.jpg"
-import inside7 from "../images/_inside7.jpg"
-import inside8 from "../images/_inside8.jpg"
-import inside9 from "../images/_inside9.jpg"
-import inside10 from "../images/_inside10.jpg"
-import inside11 from "../images/_inside11.jpg"
-import inside12 from "../images/_inside12.jpg"
-import inside13 from "../images/_inside13.jpg"
-import inside14 from "../images/_inside14.jpg"
-import inside15 from "../images/_inside15.jpg"
-import inside16 from "../images/_inside16.jpg"
-import inside17 from "../images/_inside17.jpg"
-import inside18 from "../images/_inside18.jpg"
-import inside19 from "../images/_inside19.jpg"
-import inside20 from "../images/_inside20.jpg"
-import inside21 from "../images/_inside21.jpg"
-import inside22 from "../images/_inside22.jpg"
+import inside1 from "../images-sm/_inside1.webp"
+import inside2 from "../images-sm/_inside2.webp"
+import inside3 from "../images-sm/_inside3.webp"
+import inside4 from "../images-sm/_inside4.webp"
+import inside5 from "../images-sm/_inside5.webp"
+import inside6 from "../images-sm/_inside6.webp"
+import inside7 from "../images-sm/_inside7.webp"
+import inside8 from "../images-sm/_inside8.webp"
+import inside9 from "../images-sm/_inside9.webp"
+import inside10 from "../images-sm/_inside10.webp"
+import inside11 from "../images-sm/_inside11.webp"
+import inside12 from "../images-sm/_inside12.webp"
+import inside13 from "../images-sm/_inside13.webp"
+import inside14 from "../images-sm/_inside14.webp"
+import inside15 from "../images-sm/_inside15.webp"
+import inside16 from "../images-sm/_inside16.webp"
+import inside17 from "../images-sm/_inside17.webp"
+import inside18 from "../images-sm/_inside18.webp"
+import inside19 from "../images-sm/_inside19.webp"
+import inside20 from "../images-sm/_inside20.webp"
+import inside21 from "../images-sm/_inside21.webp"
+import inside22 from "../images-sm/_inside22.webp"
 
 const insideImages = [
   inside1,
@@ -119,31 +95,6 @@ const framesText = [
   { img: framesText22, link: "" },
 ]
 
-const frames = [
-  frames1,
-  frames2,
-  frames3,
-  frames4,
-  frames5,
-  frames6,
-  frames7,
-  frames8,
-  frames9,
-  frames10,
-  frames11,
-  frames12,
-  frames13,
-  frames14,
-  frames15,
-  frames16,
-  frames17,
-  frames18,
-  frames19,
-  frames20,
-  frames21,
-  frames22,
-]
-
 // hex colors
 const hexColors = [
   "#1be4e8",
@@ -176,7 +127,6 @@ export default function Page() {
   return (
     <Home
       insideImages={insideImages}
-      frames={frames}
       framesText={framesText}
       hexColors={hexColors}
     />
--- src/components/home.js
+++ src/components/home.js
@@ -5,13 +5,12 @@ import styles from "./home.module.css"
 import { useScrollIndex } from "./useScrollIndex"
 import { useRef, useEffect } from "react"
 
-export default function Home({ insideImages, frames, framesText, hexColors }) {
+export default function Home({ insideImages, framesText, hexColors }) {
   const [insideRef, scrollIdx, scrollFrac] = useScrollIndex(insideImages.length)
 
   const getFrameOpacity = (idx) => {
     return Math.round(scrollFrac) === idx ? 1 : 0
   }
-  // console.log("scrollIdx, scrollFrac", scrollIdx, scrollFrac)
 
   const gradientRef = useRef(null)
 
@@ -26,48 +25,8 @@ export default function Home({ insideImages, frames, framesText, hexColors }) {
     return () => inside.removeEventListener("scroll", syncScroll)
   }, [insideRef])
 
-  // // Calculate opacity for each frame based on scrollIdx and its fractional position
-  // function getFrameOpacity(idx) {
-  //   // Only the closest index is visible, but use scrollFrac for future blending if needed
-  //   return Math.round(scrollFrac) === idx ? 1 : 0
-  // }
-
-  // // Helper to blend two hex colors
-  // function blendHexColors(hex1, hex2, t) {
-  //   // Remove #
-  //   hex1 = hex1.replace("#", "")
-  //   hex2 = hex2.replace("#", "")
-  //   // Parse r,g,b
-  //   const r1 = parseInt(hex1.substring(0, 2), 16)
-  //   const g1 = parseInt(hex1.substring(2, 4), 16)
-  //   const b1 = parseInt(hex1.substring(4, 6), 16)
-  //   const r2 = parseInt(hex2.substring(0, 2), 16)
-  //   const g2 = parseInt(hex2.substring(2, 4), 16)
-  //   const b2 = parseInt(hex2.substring(4, 6), 16)
-  //   // Interpolate
-  //   const r = Math.round(r1 + (r2 - r1) * t)
-  //   const g = Math.round(g1 + (g2 - g1) * t)
-  //   const b = Math.round(b1 + (b2 - b1) * t)
-  //   return `#${r.toString(16).padStart(2, "0")}${g
-  //     .toString(16)
-  //     .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`
-  // }
-
-  // // Calculate color transition progress between current and next image using scrollFrac
-  // const idx = Math.floor(scrollFrac)
-  // const frac = Math.min(1, Math.max(0, scrollFrac - idx))
-  // let color = hexColors[idx]
-  // if (idx < hexColors.length - 1 && frac >= 0.5) {
-  //   // Start blending at 50%
-  //   const blendT = (frac - 0.5) * 2 // 0 at 50%, 1 at 100%
-  //   color = blendHexColors(hexColors[idx], hexColors[idx + 1], blendT)
-  // }
-
   return (
-    <div
-      className={styles.page}
-      // style={{ "--main-color": color }}
-    >
+    <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.gradientOverlay} ref={gradientRef}>
           {insideImages.map(({}, idx) => (
@@ -86,45 +45,24 @@ export default function Home({ insideImages, frames, framesText, hexColors }) {
               key={`inside-${idx}`}
               src={src}
               alt={`Inside ${idx + 1}`}
-              width={4000}
-              height={2250}
-              loading="lazy"
-              placeholder="blur"
+              width={2000}
+              height={1125}
+              priority={true}
+              // loading="lazy"
+              // placeholder="blur"
             />
           ))}
         </div>
-        {/* <div className={styles.hexGrey}>
-          <div />
-          <div />
-        </div>
-         */}
         <div className={styles.hexColors}>
           <div />
           <div />
           <div />
         </div>
-        {/* <div className={styles.hexColor}></div> */}
         <div className={styles.hexGrey}>
           <div />
           <div />
         </div>
-        {/* <div className={styles.frames}>
-          {frames.map((src, idx) => (
-            <Image
-              key={idx}
-              src={src}
-              alt={`Frame ${idx + 1}`}
-              width={4000}
-              height={2250}
-              style={{
-                opacity: getFrameOpacity(idx),
-                transition: "opacity 2s",
-              }}
-            />
-          ))}
-        </div> */}
-        {/*
-         */}
+
         <div className={styles.framesText}>
           {framesText.map(({ img, link }, idx) => {
             const isActive = getFrameOpacity(idx) === 1
@@ -140,8 +78,9 @@ export default function Home({ insideImages, frames, framesText, hexColors }) {
                 <Image
                   key={idx}
                   src={img}
-                  width={4000}
-                  height={2250}
+                  width={2000}
+                  height={1125}
+                  priority={true}
                   alt={`Frame ${idx + 1}`}
                 />
               </div>
@@ -149,50 +88,6 @@ export default function Home({ insideImages, frames, framesText, hexColors }) {
           })}
         </div>
       </main>
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
     </div>
   )
 }
